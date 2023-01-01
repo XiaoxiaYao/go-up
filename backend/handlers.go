2010a1ff8658caa6c4c41d3e53530b13d3aa7cde
MichaelYao <michaelyaonz@gmail.com>
2023-01-01 08:46:49 +0800

    Try out sessions.

--- backend/handlers.go
+++ backend/handlers.go
@@ -3,8 +3,16 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"time"
 )
 
 func (app *application) Home(w http.ResponseWriter, r *http.Request) {
+	if app.Session.Exists(r.Context(), "test") {
+		msg := app.Session.GetString(r.Context(), "test")
+		fmt.Fprint(w, msg)
+		return
+	} else {
+		app.Session.Put(r.Context(), "test", "hit this page"+time.Now().UTC().String())
+	}
 	fmt.Fprint(w, "this is the home page")
 }
