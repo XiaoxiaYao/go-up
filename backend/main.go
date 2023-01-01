b0e971c211cad55f3fd68b3d3ea54a7ea1d91a12
MichaelYao <michaelyaonz@gmail.com>
2023-01-01 11:33:17 +0800

    Add some db operation method.

--- backend/main.go
+++ backend/main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"database/sql"
+	"flag"
 	"log"
 	"net/http"
 
@@ -8,19 +10,31 @@ import (
 )
 
 type application struct {
+	DSN     string
+	DB      *sql.DB
 	Session *scs.SessionManager
 }
 
 func main() {
 	app := application{}
 
+	flag.StringVar(&app.DSN, "dsn", "host=localhost port=5432 user=postgres password=postgres dbname=users sslmode=disable timezone=UTC connect_timeout=5", "Postgre connection")
+	flag.Parse()
+
+	conn, err := app.connectToDB()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	app.DB = conn
+
 	app.Session = getSession()
 
 	mux := app.routes()
 
-	log.Println("Starting server on port 8080...")
+	log.Println("✅ Starting server on port 8080. :)")
 
-	err := http.ListenAndServe(":8080", mux)
+	err = http.ListenAndServe(":8080", mux)
 	if err != nil {
 		log.Fatal(err)
 	}
