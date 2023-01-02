081ad49afd1f4eaffc7eef48b7b3e8ee06f6d802
MichaelYao <michaelyaonz@gmail.com>
2023-01-02 11:51:39 +0800

    Test this middleware too.

--- backend/middleware_test.go
+++ backend/middleware_test.go
@@ -1,17 +1,19 @@
 package main
 
 import (
+	"backend/pkg/data"
+	"fmt"
 	"net/http"
 	"net/http/httptest"
 	"testing"
 )
 
 func Test_app_enableCORS(t *testing.T) {
-	nextHandler := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request){})
+	nextHandler := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {})
 
-	var tests = []struct{
-		name string
-		method string
+	var tests = []struct {
+		name         string
+		method       string
 		expectHeader bool
 	}{
 		{"preflight", "OPTIONS", true},
@@ -32,6 +34,49 @@ func Test_app_enableCORS(t *testing.T) {
 
 		if !e.expectHeader && rr.Header().Get("Access-Control-Allow-Credentials") != "" {
 			t.Errorf("%s: expected no header, but got one", e.name)
+		}
+	}
+}
+
+func Test_app_authRequired(t *testing.T) {
+	nextHandler := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {})
+
+	testUser := data.User{
+		ID:        1,
+		FirstName: "Admin",
+		LastName:  "User",
+		Email:     "admin@example.com",
+	}
+
+	tokens, _ := app.generateTokenPair(&testUser)
+
+	var tests = []struct {
+		name             string
+		token            string
+		expectAuthorized bool
+		setHeader        bool
+	}{
+		{name: "valid token", token: fmt.Sprintf("Bearer %s", tokens.Token), expectAuthorized: true, setHeader: true},
+		{name: "no token", token: "", expectAuthorized: false, setHeader: false},
+		{name: "invalid token", token: fmt.Sprintf("Bearer %s", expiredToken), expectAuthorized: false, setHeader: true},
+	}
+
+	for _, e := range tests {
+		req, _ := http.NewRequest("GET", "/", nil)
+		if e.setHeader {
+			req.Header.Set("Authorization", e.token)
+		}
+		rr := httptest.NewRecorder()
+
+		handlerToTest := app.authRequired(nextHandler)
+		handlerToTest.ServeHTTP(rr, req)
+
+		if e.expectAuthorized && rr.Code == http.StatusUnauthorized {
+			t.Errorf("%s: got code 401, and should not have", e.name)
+		}
+
+		if !e.expectAuthorized && rr.Code != http.StatusUnauthorized {
+			t.Errorf("%s: did not get code 401, and should have", e.name)
 		}
 	}
 }
