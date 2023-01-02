4821847d59d7346d914258a3c5ba6610af63d8e1
MichaelYao <michaelyaonz@gmail.com>
2023-01-02 11:17:20 +0800

    Write unit tests for auth.

--- backend/setup_test.go
+++ backend/setup_test.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"backend/pkg/repository/dbrepo"
 	"os"
 	"testing"
 )
@@ -8,5 +9,8 @@ import (
 var app application
 
 func TestMain(m *testing.M) {
+	app.DB = &dbrepo.TestDBRepo{}
+	app.Domain = "example.com"
+	app.JWTSecret = "2dce505d96a53c5768052ee90f3df2055657518dad489160df9913f66042e160"
 	os.Exit(m.Run())
 }
