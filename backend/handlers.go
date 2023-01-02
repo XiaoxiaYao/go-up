5ead768ca14a4f754021dc7657325deb86385fe9
MichaelYao <michaelyaonz@gmail.com>
2023-01-02 10:13:38 +0800

    Implement auth handler.

--- backend/handlers.go
+++ backend/handlers.go
@@ -1,7 +1,10 @@
 package main
 
 import (
+	"errors"
 	"net/http"
+
+	"golang.org/x/crypto/bcrypt"
 )
 
 func (app *application) Home(w http.ResponseWriter, r *http.Request) {
@@ -14,8 +17,44 @@ func (app *application) Home(w http.ResponseWriter, r *http.Request) {
 	_ = app.writeJSON(w, http.StatusOK, payload)
 }
 
-func (app *application) authenticate(w http.ResponseWriter, r *http.Request) {
+type Credentials struct {
+	Username string `json:"email"`
+	Password string `json:"password"`
+}
 
+func (app *application) authenticate(w http.ResponseWriter, r *http.Request) {
+	var creds Credentials
+
+	// read a json payload
+	err := app.readJSON(w, r, &creds)
+	if err != nil {
+		app.errorJSON(w, errors.New("unauthorized"), http.StatusUnauthorized)
+		return
+	}
+
+	// look up the user by email address
+	user, err := app.DB.GetUserByEmail(creds.Username)
+	if err != nil {
+		app.errorJSON(w, errors.New("unauthorized"), http.StatusUnauthorized)
+		return
+	}
+
+	// check password
+	err = bcrypt.CompareHashAndPassword([]byte(user.Password), []byte(creds.Password))
+	if err != nil {
+		app.errorJSON(w, errors.New("unauthorized"), http.StatusUnauthorized)
+		return
+	}
+
+	// generate tokens
+	tokenPairs, err := app.generateTokenPair(user)
+	if err != nil {
+		app.errorJSON(w, errors.New("unauthorized"), http.StatusUnauthorized)
+		return
+	}
+
+	// send token to user
+	_ = app.writeJSON(w, http.StatusOK, tokenPairs)
 }
 
 func (app *application) refresh(w http.ResponseWriter, r *http.Request) {
