f8d34ce18aecad2356b01fa335be533ad1b7696f
MichaelYao <michaelyaonz@gmail.com>
2023-01-03 10:13:54 +0800

    Create refresh token.

--- backend/handlers.go
+++ backend/handlers.go
@@ -3,7 +3,10 @@ package main
 import (
 	"errors"
 	"net/http"
+	"strconv"
+	"time"
 
+	"github.com/golang-jwt/jwt/v4"
 	"golang.org/x/crypto/bcrypt"
 )
 
@@ -20,6 +23,10 @@ func (app *application) Home(w http.ResponseWriter, r *http.Request) {
 type Credentials struct {
 	Username string `json:"email"`
 	Password string `json:"password"`
+}
+
+type RefreshToken struct {
+	RefreshToken string `json:"refresh_token"`
 }
 
 func (app *application) authenticate(w http.ResponseWriter, r *http.Request) {
@@ -58,7 +65,63 @@ func (app *application) authenticate(w http.ResponseWriter, r *http.Request) {
 }
 
 func (app *application) refresh(w http.ResponseWriter, r *http.Request) {
+	var refreshToken RefreshToken
 
+	// read a json payload
+	err := app.readJSON(w, r, &refreshToken)
+	if err != nil {
+		app.errorJSON(w, errors.New("no refresh token"), http.StatusBadRequest)
+		return
+	}
+
+	claims := &Claims{}
+
+	_, err = jwt.ParseWithClaims(refreshToken.RefreshToken, claims, func(token *jwt.Token) (interface{}, error) {
+		return []byte(app.JWTSecret), nil
+	})
+
+	if err != nil {
+		app.errorJSON(w, err, http.StatusBadRequest)
+		return
+	}
+
+	if time.Unix(claims.ExpiresAt.Unix(), 0).Sub(time.Now()) > 30*time.Second {
+		app.errorJSON(w, errors.New("refresh token does not need renewed yet"), http.StatusTooEarly)
+		return
+	}
+
+	// get the user id from the claims
+	userID, err := strconv.Atoi(claims.Subject)
+	if err != nil {
+		app.errorJSON(w, err, http.StatusBadRequest)
+		return
+	}
+
+	user, err := app.DB.GetUser(userID)
+	if err != nil {
+		app.errorJSON(w, errors.New("unknown user"), http.StatusBadRequest)
+		return
+	}
+
+	tokenPairs, err := app.generateTokenPair(user)
+	if err != nil {
+		app.errorJSON(w, err, http.StatusBadRequest)
+		return
+	}
+
+	http.SetCookie(w, &http.Cookie{
+		Name:     "__Host-refresh_token",
+		Path:     "/",
+		Value:    tokenPairs.RefreshToken,
+		Expires:  time.Now().Add(refreshTokenExpiry),
+		MaxAge:   int(refreshTokenExpiry.Seconds()),
+		SameSite: http.SameSiteStrictMode,
+		Domain:   "localhost",
+		HttpOnly: true,
+		Secure:   true,
+	})
+
+	_ = app.writeJSON(w, http.StatusOK, tokenPairs)
 }
 
 func (app *application) allUsers(w http.ResponseWriter, r *http.Request) {
